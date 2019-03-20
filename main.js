bd4c0c4a124176032431a455bbe3dbbb87c95fd4
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-20 16:31:40 -0300

    github pages commit.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const menu = document.getElementById('menu-mobile');
 const ulMenu = document.getElementById('menu');
 const icon = document.getElementById('icon-menu');
 
-menu.addEventListener('click', e =>{
+menu.addEventListener('click', e => {
     e.preventDefault();
     ulMenu.classList.toggle('hide-menu');
     icon.classList.toggle('fa-align-right');
